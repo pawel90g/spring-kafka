92343f3fe9729847b41ef41587ab826c6c8c2197
Paweł Garbacik <pawel.garbacik@capgemini.com>
2021-09-22 12:18:06 +0200

    Headers logging

--- consumer/src/main/java/eu/garbacik/consumer/services/Consumer.java
+++ consumer/src/main/java/eu/garbacik/consumer/services/Consumer.java
@@ -2,6 +2,9 @@ package eu.garbacik.consumer.services;
 
 import lombok.extern.slf4j.Slf4j;
 import org.springframework.kafka.annotation.KafkaListener;
+import org.springframework.kafka.support.KafkaHeaders;
+import org.springframework.messaging.handler.annotation.Header;
+import org.springframework.messaging.handler.annotation.Payload;
 import org.springframework.stereotype.Component;
 
 @Component
@@ -9,7 +12,12 @@ import org.springframework.stereotype.Component;
 public class Consumer {
 
     @KafkaListener(id = "consumer-1", topics = "#{ @kafkaSettings.getTopic().getName() }")
-    public void listen(String message){
-        log.info("Message received: " + message);
+    public void listen(
+            @Payload String message,
+            @Header(KafkaHeaders.RECEIVED_PARTITION_ID) int partition,
+            @Header(KafkaHeaders.OFFSET) int offset) {
+        log.info("Message received: " + message +
+                ", partition: " + partition +
+                ", offset: " + offset);
     }
 }
